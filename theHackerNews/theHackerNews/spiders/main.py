842fb64b27bd7600e9410fee7a362bb305478fe4
Zahra Arshia <fzarshia@gmail.com>
2023-12-12 10:54:03 +0330

    set spider configues

--- theHackerNews/theHackerNews/spiders/main.py
+++ theHackerNews/theHackerNews/spiders/main.py
@@ -1,10 +1,59 @@
 import scrapy
+import re
 
+regex = re.compile(r"<[^>]+>")
+
+def remove_html(string):
+    return regex.sub('', string)
 
 class MainSpider(scrapy.Spider):
     name = 'main'
     allowed_domains = ['www.thehackernews.com']
-    start_urls = ['http://www.thehackernews.com/']
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url='https://thehackernews.com', 
+            callback=self.parse, 
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+            },
+            meta = {
+                'first': True
+            }
+            )
 
     def parse(self, response):
-        pass
+        links = response.xpath('//a[contains(@class, "story-link")]/@href')
+        for link in links:
+            yield response.follow(
+                url = link.get(),
+                callback = self.thred,
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+                },
+                meta={
+                    'link': link.get(),
+                    'first': True
+                }
+            )
+
+            next_page = response.xpath('(//a[contains(@class, "blog-pager-older-link-mobile")])/@href').get()
+            yield response.follow(
+                url = next_page,
+                callback = self.parse,
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+                },
+                meta={
+                    'first': False
+                }
+            )
+
+    def thred(self, response):
+        articles = response.xpath('//div[contains(@class, "articlebody")]')
+        for article in articles:
+            yield{
+                'article': remove_html(article.get()),
+                'link': response.request.meta['link']
+            }
+
