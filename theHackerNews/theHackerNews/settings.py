6337868b127c9f3d7ca71c77f402178611542746
Zahra Arshia <fzarshia@gmail.com>
2024-01-23 12:39:39 +0330

    Update settings.py

--- theHackerNews/theHackerNews/settings.py
+++ theHackerNews/theHackerNews/settings.py
@@ -25,7 +25,8 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = .25
+RANDOMIZE_DOWNLOAD_DELAY = True
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -92,5 +93,5 @@ MONGO_DATABASE = 'mongo-cti'
 REQUEST_FINGERPRINTER_IMPLEMENTATION = '2.7'
 TWISTED_REACTOR = 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'
 AUTOTHROTTLE_ENABLED = True
-HTTPCACHE_ENABLED = True
+HTTPCACHE_ENABLED = False
 FEED_EXPORT_ENCODING = 'utf-8'
